036c2f498a1d0dca08e6b01bc55c1ccff36681e1
jinghewang <jinghewang@163.com>
2017-11-08 11:35:28 +0800

    add entry ok

--- src/frontend/views/site/entry.php
+++ src/frontend/views/site/entry.php
@@ -1,15 +1,17 @@
 <?php
+
 use yii\widgets\ActiveForm;
 use \yii\helpers\Html;
+
 ?>
 
 <?php $form = ActiveForm::begin(); ?>
 
-<?= $form->field($form, 'name') ?>
-<?= $form->field($form, 'email') ?>
+<?= $form->field($model, 'name') ?>
+<?= $form->field($model, 'email') ?>
 
-<div class="form-group">
-    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
-</div>
+    <div class="form-group">
+        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+    </div>
 
 <?php ActiveForm::end(); ?>
