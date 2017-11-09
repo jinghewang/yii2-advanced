dd6cecd6c5bc0fd0c11a369b4be103c06050054d
jinghewang <jinghewang@163.com>
2017-11-09 09:40:33 +0800

    add person

---
+++ src/frontend/models/Person.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "person".
+ *
+ * @property string $code
+ * @property string $name
+ * @property integer $population
+ */
+class Person extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'person';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['code', 'name'], 'required'],
+            [['population'], 'integer'],
+            [['code'], 'string', 'max' => 2],
+            [['name'], 'string', 'max' => 52],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'code' => 'Code',
+            'name' => 'Name',
+            'population' => 'Population',
+        ];
+    }
+}
